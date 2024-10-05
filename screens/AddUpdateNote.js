37e58401ba8e6d93bfbb5afd86c070c28316b67e
maiquoctruong <nguyenthanhtung0900@gmail.com>
2024-10-06 04:06:24 +0700

    ui for lab7 apinotes

---
+++ screens/AddUpdateNote.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import axios from 'axios';
+
+function AddUpdateNote() {
+    const [content, setContent] = useState('');
+    const [date, setDate] = useState('');
+
+    const handleSubmit = async () => {
+        await axios.post('http://localhost:5000/notes', { content, date });
+        Alert.alert('Thành công', 'Thêm ghi chú thành công');
+        setContent('');
+        setDate('');
+    };
+
+    return (
+        <View style={{ padding: 20 }}>
+            <Text>Thêm Ghi Chú</Text>
+            <TextInput
+                placeholder="Nội dung"
+                value={content}
+                onChangeText={setContent}
+                style={{ borderWidth: 1, marginBottom: 10 }}
+            />
+            <TextInput
+                placeholder="Ngày"
+                value={date}
+                onChangeText={setDate}
+                style={{ borderWidth: 1, marginBottom: 10 }}
+            />
+            <Button title="Thêm" onPress={handleSubmit} />
+        </View>
+    );
+}
+
+export default AddUpdateNote;
